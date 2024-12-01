84beec2dd0aefce98a78bad8279e85d4c30c4f98
shanizu <77214955+shanizu@users.noreply.github.com>
2024-12-01 16:30:08 +0200

    updates pin colors

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,5 +1,10 @@
 // import { useEffect, useRef } from "react";
-import { APIProvider, Map, AdvancedMarker } from "@vis.gl/react-google-maps";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+} from "@vis.gl/react-google-maps";
 import { Waypoint } from "../types";
 import { Polyline } from "./Polyline";
 
@@ -12,24 +17,29 @@ const MapComponent = ({ waypoints, route }: MapComponentProps) => {
   return (
     <APIProvider apiKey={import.meta.env.VITE_MAPS_API_KEY}>
       <Map
-        // style={{ width: "50vh", height: "50vh" }}
         defaultZoom={10}
         defaultCenter={{ lat: 37.7749, lng: -122.4194 }}
         mapId={"DEMO_MAP_ID"}
       >
+        <Polyline
+          strokeWeight={3}
+          strokeColor={"#0d6efd"}
+          encodedPath={route}
+        />
         {waypoints.map((waypoint, index) => (
           <AdvancedMarker
             position={{
               lat: waypoint.location.latitude,
               lng: waypoint.location.longitude,
             }}
-          />
+          >
+            <Pin
+              background={"#0d6efd"}
+              borderColor={"#FFFFFF"}
+              glyphColor={"#FFFFFF"}
+            />
+          </AdvancedMarker>
         ))}
-        <Polyline
-          strokeWeight={3}
-          strokeColor={"#0d6efd"}
-          encodedPath={route}
-        />
       </Map>
     </APIProvider>
   );
